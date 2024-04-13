476910f5ce2a0bad5705a9dbc6497cb80d49f4d6
mgsulaiman <info@mgsulaiman.com>
2024-04-13 15:27:50 +0300

    some update

---
+++ Test/Analytics/Sources/Analytics/TestFile.swift
@@ -0,0 +1,14 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by mgsulaimanCom on 13/04/2024.
+//
+
+import Foundation
+public class TestFile {
+    public func returnString() -> String {
+        let myName = "Mgsulaiman"
+        return myName
+    }
+}
